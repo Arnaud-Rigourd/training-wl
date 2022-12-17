f88c037387a6e6eb7d23c03ede08182bd0ac2f9f
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-12-17 17:10:18 +0100

    Add faker and create seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,9 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
 
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
-  validates :category, presence: true, inclusion: CATEGORY
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :address, presence: true
 
 end
